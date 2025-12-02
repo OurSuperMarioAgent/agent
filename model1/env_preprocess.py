c20caa676da9b5636078c9612dd5b041d94cdbef
cooperr3165 <15350787266@163.com>
2025-12-02 18:04:30 +0800

    第一个模型

---
+++ model1/env_preprocess.py
@@ -0,0 +1,18 @@
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym.wrappers import GrayScaleObservation,ResizeObservation
+from skip_frame_wrapper import SkipFrameWrapper
+from stable_baselines3 import PPO
+from super_parameters import *
+from reward_wrapper import CustomRewardWrapper, SparseToDenseRewardWrapper
+
+def create_env():
+    env = gym_super_mario_bros.make('SuperMarioBros-v2')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = GrayScaleObservation(env, keep_dim=True)
+    env = SparseToDenseRewardWrapper(env)
+    env = CustomRewardWrapper(env)
+    env = ResizeObservation(env, resize_observation_shape)
+    env = SkipFrameWrapper(env, skip=n_frame_skip)
+    return env
